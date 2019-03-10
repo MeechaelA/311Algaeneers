8e5246e9fe07fa3a6948569d8b29285ec1b01fb9
Brady Burnsides <brady@burnsides.us>
2019-03-09 19:18:14 -0600

    Added comments
    Commented file for clarification on functionality

--- DataLogging.py
+++ DataLogging.py
@@ -1,12 +1,16 @@
 import serial
 import datetime
 
-currTime = str(datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S"))
+# Create string with format 2019-3-09_19.15.02.csv
+currTime = str(datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S"))
 filename = str(currTime + ".csv")
 
+# Create file with time stamp name and add column headers for easier analysis
 startFile = open(filename, 'w+')
 startFile.write("Time [ms],CO2 [ppm],Temp [dC],Heatpad (ON/OFF)")
 
+# This COM port may need to be changed before the script works properly. Check Arduino IDE. For Windows it'll likely be
+# COM6 or COM3. Also, make sure .ino baud rate and baud rate here agree.
 ser = serial.Serial('COM6', 9600)
 
 while True:
@@ -14,5 +18,6 @@ while True:
 
 	print(line) # Display outputs in console for viewing
 
+	# Append data to .csv file
 	dataAppend = open(filename, 'a')
 	dataAppend.write(line)
